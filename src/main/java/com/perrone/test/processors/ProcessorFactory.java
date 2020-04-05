af5dce17a3f352229678f748ea4f1bf8af9b3454
cschellberg <dschellberg@gmail.com>
2020-04-05 07:59:10 -0400

    added processor caching to eliminate unnecessary object creation

--- src/main/java/com/perrone/test/processors/ProcessorFactory.java
+++ src/main/java/com/perrone/test/processors/ProcessorFactory.java
@@ -1,26 +1,38 @@
 package com.perrone.test.processors;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.perrone.test.Constants;
 import com.perrone.test.exceptions.UnsupportedProcessorType;
 
 /**
- * @author dschellb
- * Factory for creating a Processor based on the processor type
+ * @author dschellb Factory for creating a Processor based on the processor type
  *
  */
 public class ProcessorFactory {
+
+	private static Map<Byte, Processor> processorCache = new ConcurrentHashMap<>();
 
 	/**
 	 * @param processorType
 	 * @return Processor
 	 */
 	public static Processor getProcessor(byte processorType) {
-		switch (processorType) {
-		case Constants.MATRIX_PROCESSOR:
-			return new MatrixProcessor();
-		default:
-			throw new UnsupportedProcessorType("Could not find processor type " + processorType);
-		}
+		return getProcessorFromCache(processorType);
+	}
+
+	private static Processor getProcessorFromCache(byte processorType) {
+		Byte processorTypeObject = Byte.valueOf(processorType);
+		return processorCache.computeIfAbsent(processorTypeObject, pt -> {
+			switch (pt) {
+			case Constants.MATRIX_PROCESSOR:
+				return new MatrixProcessor();
+			default:
+				throw new UnsupportedProcessorType("Could not find processor type " + processorType);
+			}
+		});
+
 	}
 
 }
